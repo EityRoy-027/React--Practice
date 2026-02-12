b12fcc739d421f9e3d83eba4111e9417f11afb1d
eityroy <eityroy027@gmail.com>
2026-02-12 22:39:53 +0600

    react

--- my-react-app/src/Destructure.jsx
+++ my-react-app/src/Destructure.jsx
@@ -1,16 +1,60 @@
 
 export default function List({task,isDone,month}){
   
+
+// if/else:    
     
 if(isDone===true){
     return(
         <li>Done:{task}</li>
     )
 }    
-else{
+// else{
+//     return(
+//           <li>Processing:{task} Starting-month:{month}</li>
+//     )
+// }
+
+//return=null:-
+
+if(isDone===true){
     return(
-          <li>Processing:{task} Starting-month:{month}</li>
+        null
+    )
+}   
+
+//ternary:-
+
+return(
+    isDone?  <li>Done:{task}</li> :<li>Processing:{task} Starting-month:{month}</li>
+)
+
+//conditional randaring:-
+
+ isDone && <li>Done:{task} month:{month}</li>//only when true
+
+
+ isDone || <li>Done:{task} month:{month}</li>//only when false
+
+
+
+ //using variable:-
+
+let listItem;
+
+
+if(isDone===true){
+    return(
+      listItem=  <li>Done:{task}</li>
     )
 }
+
+else{
+    listItem=<li>Pending:{task}</li>
+}
+
+return(
+    listItem
+)
 
 }
